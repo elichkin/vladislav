fb7ca8b24a19418747f0f2dfd82d01fb70040235
elichkin <145639440+elichkin@users.noreply.github.com>
2023-10-06 16:13:43 +0500

    Add files via upload

---
+++ Samost4_5_Calculation_2.py
@@ -0,0 +1,7 @@
+from Samost4_5_Сalculation_1 import calc_truangle_square_gerone_calc
+
+a = int(input('Введите сторону треугольника а: '))
+b = int(input('Введите сторону треугольника b: '))
+c = int(input('Введите сторону треугольника c: '))
+
+print('S =', calc_truangle_square_gerone_calc(a, b, c))
