87bae3b0854913719dfd5f49753dacaa14cb8120
elichkin <145639440+elichkin@users.noreply.github.com>
2023-10-07 01:55:59 +0500

    Add files via upload

---
+++ Samost8_1.py
@@ -0,0 +1,7 @@
+class Animal:
+    def __init__(self, name, weight):
+        self.name = name
+        self.weight = weight
+my_animal = Animal('Diper', 28)
+print(my_animal.name)
+print(my_animal.weight)
