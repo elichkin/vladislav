a61b564c626dfaaa18544868798b53039f0df8df
elichkin <145639440+elichkin@users.noreply.github.com>
2023-10-06 21:56:17 +0500

    Add files via upload

---
+++ Samost6_2.py
@@ -0,0 +1,18 @@
+example_1 = ((1, 2, 3), 1)
+example_2 = ((1, 2, 3, 1, 2, 3, 4, 5, 2, 3, 4, 2, 4, 2), 3)
+example_3 = ((2, 4, 6, 6, 4, 2), 9)
+
+def remove_element_from_tuple(value):
+    source_tuple, element = value
+    new_list = []
+    was_removed = False
+    for x in list(source_tuple):
+        if x == element and was_removed == False:
+            was_removed = True
+            continue
+        new_list.append(x)
+    return tuple(new_list)
+
+print('Результат #1:', remove_element_from_tuple((example_1)))
+print('Результат #2:', remove_element_from_tuple((example_2)))
+print('Результат #3:', remove_element_from_tuple((example_3)))
