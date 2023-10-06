6d8516baa15653c8683431234d87230317a5b84d
elichkin <145639440+elichkin@users.noreply.github.com>
2023-10-06 21:29:39 +0500

    Add files via upload

---
+++ Samost6_1.py
@@ -0,0 +1,4 @@
+value = input('Пожалуйста, укажите свою последовательность чисел: ')
+processed_value = value.split(' ')
+print('Список:', list(processed_value))
+print('Кортеж:', tuple(processed_value))
