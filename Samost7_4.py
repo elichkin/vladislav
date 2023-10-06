05b1e31151aacef6a3aeada2e5a0d26ba29ea8cb
elichkin <145639440+elichkin@users.noreply.github.com>
2023-10-07 00:28:57 +0500

    Add files via upload

---
+++ Samost7_4.py
@@ -0,0 +1,15 @@
+example_1 = 'Hello, world! Python IS the programming language of thE future. My\nEMAIL is....\nPYTHON is awesome!!!!'
+
+with open('./txtfiles/Samost7_4.txt', 'r') as file:
+    words = file.read().split(' ')
+    lower_text = example_1.lower()
+    for word in words:
+        lower_text = lower_text.replace(word, '*' * len(word))
+    result = ''
+    for index, word in enumerate(lower_text):
+        if example_1[index].lower() == word:
+            result += example_1[index]
+        else:
+            result += word
+
+    print(result)
